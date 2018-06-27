25d32ccea86cdb53ec9f47bd6671a92a1ad68532
Unknown <dlalsdk12321@naver.com>
2018-06-27 16:28:25 +0900

    1회차 : HelloWorld 예제

---
+++ src/Day1/HelloWorld.java
@@ -0,0 +1,31 @@
+package Day1;
+
+/*
+ * public : 접근제한자, 누구나 다 접근가능 (public이외에도 접근제한자 단어가 있다)
+ * 접근제한자는 외부에서 해당하는 클래스 또는 메소드에 접근을 허용할지 말지를 결정
+ * class : 클래스임을 나타낸다.
+ * 색깔이 보라색 = 키워드또는 명령어
+ * {} : 애네는 항상 세트로 있어야한다.
+ * 중가로 안에 있는 것은 HelloWorld가 같고 있는 것이다. 왜냐 헬로우 월드 뒤에 있으니까.
+ * */
+
+public class HelloWorld {
+
+	/*
+	 * static : 클래스를 통해 호출함
+	 * void : 반환이 없음, 메소드 설명에서 자세히 할 예정.
+	 * main : 메소드 이름
+	 * 메소드 : 특정한 기능을 하는 애들. 즉, 특별한 입력이 필요로 한다.
+	 * (string[] args) : 매개변수
+	 * 자바에서 프로그램을 실행하려면 main메소드가 있어야만 한다!
+	 * */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//위에 있는 것의 뜻은 '표준 출력 메소드'이다.
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        //ln = 엔터 역할
+	}
+
+}
